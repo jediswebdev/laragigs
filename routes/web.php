5b12c3b3f129d5b27d2ca7e50592d5faa5b3e065
jediswebdev <jediswebdev@gmail.com>
2026-08-24 15:50:19 +0100

    starting to build authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+
+// Register user
+Route::get('/register', [UserController::class, 'create'])->name('register');
+
+// Login user
+Route::get('/login', [UserController::class, 'create'])->name('login');
+
+// Create User
+Route::post('/users', [UserController::class, 'store']);
+
+
 
 // All listings
 Route::get('/', [ListingController::class, 'index'])->name('listings.index');
